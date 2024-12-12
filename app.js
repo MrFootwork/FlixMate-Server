8bb89f30100d635d287f709b53558540309f5972
Porcari-Karim <porcari.karim1@gmail.com>
2024-12-12 13:34:54 +0100

    allowed credentials in cors

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ const router = require('./features/index.routes')
 const app = express()
 app.use(logger)
 app.use(
-	cors({
-		origin: ['http://localhost:5173', 'https://flixmate-client.onrender.com'],
-	})
+  cors({
+    origin: ['http://localhost:5173', 'https://flixmate-client.onrender.com'],
+    credentials: true,
+  })
 )
 app.use(bodyParser.json())
 app.use(cookieParser())
@@ -24,5 +25,5 @@ app.use(router)
 // app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 app.listen(require('./config').PORT, _ =>
-	console.log('Listening on port: ', require('./config').PORT)
+  console.log('Listening on port: ', require('./config').PORT)
 )
