6fec6568a45f4ff06107ba196982d712197132ee
Porcari-Karim <porcari.karim1@gmail.com>
2024-12-19 15:46:03 +0100

    Added Carousel, leaving the room now deletes the user from the rooms participants if none of his sockets is connected

--- features/index.socket.js
+++ features/index.socket.js
@@ -1,6 +1,10 @@
 const http = require('http')
 const { getUserFromJWT } = require('./users/users.services')
-const { getRoombyId, addUserToRoom } = require('./rooms/rooms.services')
+const {
+  getRoombyId,
+  addUserToRoom,
+  removeUserFromRoom,
+} = require('./rooms/rooms.services')
 const { createMessage } = require('./messages/messages.services')
 
 function socketServer(app) {
@@ -61,6 +65,7 @@ function addListenersToSocket(socket, io) {
       if (dbRoom) {
         await addUserToRoom(socket.user.id, room)
         socket.room = room
+        socket.type = 'Show'
         socket.join(room)
         console.log(socket.user.name, ' joined room ', room)
         socket.emit('joined-room')
@@ -93,12 +98,59 @@ function addListenersToSocket(socket, io) {
     })
   })
 
+  socket.on('join-chat', async room => {
+    try {
+      const dbRoom = await getRoombyId(room)
+      // setTimeout(() => {
+      //   io.to(socket.room).emit('netflix-send', {
+      //     eventType: 'seeked',
+      //     videoTime: '',
+      //     eventUser: socket.user,
+      //   })
+      // }, 20000)
+
+      if (dbRoom) {
+        // await addUserToRoom(socket.user.id, room)
+        socket.room = room
+        socket.type = 'Chat'
+        socket.join(room)
+        console.log(socket.user.name, ' joined room ', room)
+        socket.emit('joined-room')
+      }
+    } catch (error) {
+      console.log(error)
+      socket.emit('error', 'The room you are trying to join does not exist!')
+    }
+  })
+
   socket.on('receive-message', message => {
     try {
+      if (!message) return
       handleMessageReceived(io, socket.room, message, socket.user)
     } catch (error) {
       console.log(error)
       socket.emit('error', error)
+    }
+  })
+
+  socket.on('disconnect', async _ => {
+    try {
+      if (socket.room) {
+        console.log(`${socket.user.name} disconnected from room ${socket.room}`)
+        const dbRoom = await getRoombyId(socket.room)
+        if (dbRoom) {
+          const roomSockets = await io.in(socket.room).fetchSockets()
+
+          const sameUserSockets = roomSockets.filter(
+            msocket => msocket.user.email === socket.user.email
+          )
+          console.log(sameUserSockets.length)
+          if (sameUserSockets.length === 0)
+            removeUserFromRoom(socket.user.id, socket.room)
+        }
+      }
+    } catch (error) {
+      console.log('Disconnect Error: ', error)
     }
   })
 }
